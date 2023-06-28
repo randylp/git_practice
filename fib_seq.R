b3b6ae3de05c7d40312a8e0f3180b9da50c72165
Adam Reimer <adam.reimer@alaska.gov>
2023-06-27 16:44:00 -0800

    Sixth number in Fibonacci Sequence
    Also fixed a typo in the header

--- fib_seq.R
+++ fib_seq.R
@@ -1,6 +1,7 @@
-#Create the Fibonacci seqence to practice git commands
+#Create the Fibonacci sequence to practice git commands
 
 fib_seq <- c(0, 1)
 fib_seq[3] <- fib_seq[1] + fib_seq[2]
 fib_seq[4] <- fib_seq[2] + fib_seq[3]
 fib_seq[5] <- fib_seq[3] + fib_seq[4]
+fib_seq[6] <- fib_seq[4] + fib_seq[5]
